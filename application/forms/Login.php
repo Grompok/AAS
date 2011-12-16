15f0ebe1fdba529873d7526ba8c54c6a186dae79
Darius <darius.lenkauskis@yahoo.com>
2011-12-16 21:51:09 +0200

    login form

--- application/forms/Login.php
+++ application/forms/Login.php
@@ -1,31 +1,50 @@
 <?php
 
-/**
- * Login Form
- *
- * @author Darius <darius.lenkauskis@yahoo.com>
- */
 class Application_Form_Login extends Zend_Form
 {
+
     public function init()
     {
+        $this->setAttrib('class', 'form-stacked');
+        
+        $this->setDecorators(array(
+            'FormElements',
+            'Form',
+        ));
+        
         //username
         $this->addElement('text', 'username', array(
             'label' => 'Slapyvardis',
             'required' => true,
             'filters' => array('StringToLower'),
-            'validators' => array('Alnum')
+            'validators' => array('Alnum'),
+            'decorators' => array(
+              'ViewHelper',
+              'Label',
+              array('HtmlTag', array('tag' => 'div', 'class' => 'clearfix')),
+            ),
         ));
-        
+
         //password
         $this->addElement('password', 'password', array(
             'label' => 'Slaptažodis',
             'required' => true,
-            'validators' => array('Alnum')
+            'validators' => array('Alnum'),
+            'decorators' => array(
+              'ViewHelper',
+              'Label',
+              array('HtmlTag', array('tag' => 'div', 'class' => 'clearfix')),
+            ),
         ));
-        
+
         $this->addElement('submit', 'Prisijungti', array(
-            
+            'style' => 'float: right;',
+            'class' => 'btn success',
+            'decorators' => 
+            array(
+              'ViewHelper',
+            ),
         ));
     }
+
 }
