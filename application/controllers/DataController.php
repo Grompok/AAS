6572c7b47be2c219208c0e9071e61a8e932c4ff2
Darius <darius.lenkauskis@yahoo.com>
2011-12-17 20:35:30 +0200

    full import done

--- application/controllers/DataController.php
+++ application/controllers/DataController.php
@@ -1,15 +1,12 @@
 <?php
 
-class DataController extends Zend_Controller_Action
-{
+class DataController extends Zend_Controller_Action {
 
-    public function indexAction()
-    {
+    public function indexAction() {
         
     }
 
-    public function importAction()
-    {
+    public function importAction() {
         $form = new Application_Form_Import();
         $data = null;
         if ($this->_request->isPost()) {
@@ -23,7 +20,7 @@ class DataController extends Zend_Controller_Action
                 //==============================================================
                 $padaliniai = new Application_Model_DbTable_Padaliniai();
 
-                for ($i = 2; $i < $data->sheets[1]['numRows']+1; $i++) {
+                for ($i = 2; $i < $data->sheets[1]['numRows'] + 1; $i++) {
                     try {
                         $padaliniai->insert(array(
                             'Pad_Kodas' => $data->sheets[1]['cells'][$i][1],
@@ -51,112 +48,114 @@ class DataController extends Zend_Controller_Action
                         //echo $zdb->getMessage();
                     }
                 }
-                
+
                 unset($is);
-                
+
                 //==============================================================
                 //                  IS && Padaliniai importavimas
                 //==============================================================
-                
+
                 $is_padaliniai = new Application_Model_DbTable_IsPadaliniai();
-                
+
                 $cols = $data->sheets[3]['numCols'] - 2;
                 for ($i = 2; $i < $cols; $i++) {
-                    for($j = 2; $j <= $cols+1; $j++) {
-                        if(isset($data->sheets[3]['cells'][$i][$j]) 
+                    for ($j = 2; $j <= $cols + 1; $j++) {
+                        if (isset($data->sheets[3]['cells'][$i][$j])
                                 && ($data->sheets[3]['cells'][$i][$j] === 1)) {
                             try {
                                 $is_padaliniai->insert(array(
-                                    'Pad_kodas' =>  $data->sheets[3]['cells'][1][$j],
+                                    'Pad_kodas' => $data->sheets[3]['cells'][1][$j],
                                     'IS_kodas' => $data->sheets[3]['cells'][$i][1],
-                                ));    
+                                ));
                             } catch (Zend_Db_Exception $zdb) {
                                 //$zdb->getMessage();
                             }
                         }
                     }
                 }
-                
+
                 unset($is_padaliniai);
-                
+
                 //==============================================================
                 //                  PriemoniuKryptis && ParaiskuPriemones
                 //==============================================================
-                
+
                 $paramosPriemones = new Application_Model_DbTable_ParamosPriemones();
                 $priemoniuKryptis = new Application_Model_DbTable_PriemoniuKryptis();
                 $kryptis = 0;
-                for($i = 2; $i <= $data->sheets[0]['numRows']; $i++) {
+                for ($i = 2; $i <= $data->sheets[0]['numRows']; $i++) {
                     try {
-                       if(isset($data->sheets[0]['cells'][$i][1])) {
-                           $paramosPriemones->insert(array(
-                               'Priem_kodas' => strtolower($data->sheets[0]['cells'][$i][1]),
-                               'Pavadinimas' => $data->sheets[0]['cells'][$i][2],
-                               'Krypt_kodas' => $kryptis,
-                           ));
-                       } else {
-                           $kryptis++; 
-                           $priemoniuKryptis->insert(array(
-                               'Krypt_kodas' => $kryptis,
-                               'Pavadinimas' => $data->sheets[0]['cells'][$i][2],
-                           ));
-                       }
-                    } catch(Zend_Db_Exception $zbe) {
+                        if (isset($data->sheets[0]['cells'][$i][1])) {
+                            $paramosPriemones->insert(array(
+                                'Priem_kodas' => strtolower($data->sheets[0]['cells'][$i][1]),
+                                'Pavadinimas' => $data->sheets[0]['cells'][$i][2],
+                                'Krypt_kodas' => $kryptis,
+                            ));
+                        } else {
+                            $kryptis++;
+                            $priemoniuKryptis->insert(array(
+                                'Krypt_kodas' => $kryptis,
+                                'Pavadinimas' => $data->sheets[0]['cells'][$i][2],
+                            ));
+                        }
+                    } catch (Zend_Db_Exception $zbe) {
                         //echo $zbe->getMessage();
                     }
                 }
                 unset($kryptis);
                 unset($paramosPriemones);
-                
+
                 //==============================================================
                 //          Priemoniu kiekis
                 //==============================================================
-                
+
                 $paramosKiekiai = new Application_Model_DbTable_ParamosKiekiai();
-                
-                for($i = 2; $i <= $data->sheets[5]['numRows']; $i++) {
+
+                for ($i = 2; $i <= $data->sheets[5]['numRows']; $i++) {
                     try {
-                        
+
                         $paramosKiekiai->insert(array(
-                            'Priem_kodas' => $data->sheets[5]['cells'][$i][1], 
+                            'Priem_kodas' => $data->sheets[5]['cells'][$i][1],
                             'Nuo' => $data->sheets[5]['cells'][$i][2],
                             'Iki' => $data->sheets[5]['cells'][$i][3],
                             'Kiekis' => $data->sheets[5]['cells'][$i][4],
                         ));
-                        
-                    } catch(Zend_Db_Exception $zbe) {
-                       //echo $zbe->getMessage();
+                    } catch (Zend_Db_Exception $zbe) {
+                        //echo $zbe->getMessage();
                     }
                 }
-                
+
                 unset($paramosKiekiai);
-                
+
                 //==============================================================
                 //          Paramos Administravimas
                 //==============================================================
                 
-                var_dump($data->sheets[4]);
+                $paramosAdministravimas = new Application_Model_DbTable_ParamosAdministravimas();
                 
-                $cols = $data->sheets[4]['numCols'] - 2;
+                $cols = $data->sheets[4]['numRows'];
                 for ($i = 2; $i < $cols; $i++) {
-                    for($j = 2; $j <= $cols+1; $j++) {
-                        if(isset($data->sheets[4]['cells'][$i][$j]) 
-                                && (is_integer($data->sheets[4]['cells'][$i][$j]) 
-                                        || is_float($data->sheets[4]['cells'][$i][$j]))) {
-                            echo $data->sheets[4]['cells'][$i][1];
-//                            try {
-//                                $is_padaliniai->insert(array(
-//                                    'Priem_kodas' =>  $data->sheets[4]['cells'][$i][$j],
-//                                    'Pad_kodas' => $data->sheets[4]['cells'][1][$j],
-//                                    'Valandos'  => $data->sheets[4]['cells'][$i][1],
-//                                ));    
-//                            } catch (Zend_Db_Exception $zdb) {
-//                                //$zdb->getMessage();
-//                            }
+                    for ($j = 2; $j < $data->sheets[4]['numCols']; $j++) {
+                        if (isset($data->sheets[4]['cells'][$i][$j])
+                                && (is_integer($data->sheets[4]['cells'][$i][$j])
+                                || is_float($data->sheets[4]['cells'][$i][$j]))) {
+                            //$data->sheets[4]['cells'][$i][$j]; //reiksme
+                            //$data->sheets[4]['cells'][$i][1]; //priemones kodas
+                            // $data->sheets[4]['cells'][1][$j]; //padalino kodas
+                            try {
+                                $paramosAdministravimas->insert(array(
+                                    'Priem_kodas' => strtolower($data->sheets[4]['cells'][$i][1]),
+                                    'Pad_kodas' => $data->sheets[4]['cells'][1][$j],
+                                    'Valandos' => $data->sheets[4]['cells'][$i][$j],
+                                ));
+                            } catch (Zend_Db_Exception $zbe) {
+                                //echo $zbe->getMessage();
+                            }
                         }
                     }
                 }
                 
+                unset($paramosAdministravimas);
             }
         }
         $this->view->form = $form;
